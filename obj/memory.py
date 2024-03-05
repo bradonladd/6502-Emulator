bc42918eefa5420177ae46c61cae6b2165251acd
laddbr <laddbr@cs.wallawalla.edu>
2024-03-04 19:19:41 -0800

    Data memory split from Instruction memory

--- obj/memory.py
+++ obj/memory.py
@@ -1,6 +1,6 @@
-class Memory():
-    #OPCODES = []
-    #VALUES = []
+class InstructionMemory():
+    OPCODES = []
+    VALUES = []
     
     def __init__(self, op, val):
         self.OPCODES = op
@@ -12,9 +12,23 @@ class Memory():
             print("0x" + str(i) + ": " + self.OPCODES[i] + " " + self.VALUES[i])
         print("##############################")
 
-    def getInstruction(programCounter):
-        return OPCODES[programCounter], VALUES[programCounter]
+    def fetch(self, programCounter):
+        return self.OPCODES[programCounter], self.VALUES[programCounter]
 
+
+class DataMemory():
+    VALUES = []
+
+    def __init__(self, val):
+        self.VALUES = val
+    
+    def printMem(self):
+        print("Data Memory")
+        for i in range(0, len(self.VALUES)):
+            print("0x" + str(i) + ": " + self.VALUES[i])
+
+    def fetch(self, programCounter):
+        return self.VALUES[programCounter]
 
     
 
--- tests.py
+++ tests.py
@@ -1,13 +1,35 @@
 import unittest
-from obj.memory import Memory
+from obj.memory import InstructionMemory, DataMemory
 
-class TestMemoryMethods(unittest.TestCase):
+class TestInstructionMemoryMethods(unittest.TestCase):
     asmTestOpcodes = ["LDU", "ADD", "SUB"]
     asmTestValues = ["#44", "#66", "#88"]
 
     def test_init(self): 
-        memorySingle = Memory(["LDU", "ADD", "SUB"], ["#44", "#66", "#88"])
+        print("                       @Instruction Memory Init:@\n")
+        memorySingle = InstructionMemory(self.asmTestOpcodes, self.asmTestValues)
         memorySingle.printMem()
+    
+    def test_fetch(self):
+        print("                       @Instruction Memory Fetch:@\n")
+        memorySingle = InstructionMemory(["LDU", "ADD", "SUB"], ["#44", "#66", "#88"])
+        opcode, value = memorySingle.fetch(1)
+        print(opcode + " " + value + "\n")
+
+class TestDataMemoryMethods(unittest.TestCase):
+    asmData = ["42", "66", "01"]
+    
+    def test_init(self):
+        print("                       @Data Memory Init:@\n")
+        dataSingle = DataMemory(self.asmData)
+        dataSingle.printMem()
+        print("\n")
+    
+    def test_fetch(self):
+        print("                       @Data Memory Fetch:@\n")
+        dataSingle = DataMemory(self.asmData)
+        value = dataSingle.fetch(0)
+        print(value + "\n")
 
 
 if __name__ == '__main__':
